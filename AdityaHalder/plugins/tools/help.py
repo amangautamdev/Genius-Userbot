c18c082b7062ece6f1ea51ca080f8f750b5e4aab
Aman Gautam <129865249+amangautamdev@users.noreply.github.com>
2024-05-11 21:25:12 +0530

    Update help.py

--- AdityaHalder/plugins/tools/help.py
+++ AdityaHalder/plugins/tools/help.py
@@ -57,12 +57,12 @@ async def help_button(client, query):
     back_match = re.match(r"help_back", query.data)
     top_text = f"""
 **🥀 Welcome To Help Menu Of
-Genius Userbot » {__version__} ✨...
+Dark Devil » {__version__} ✨...
 
 Click On Below 🌺 Buttons To
 Get Userbot Commands.
 
-🌷Powered By : [Aditya Server](https://t.me/AdityaServer).**
+🌷Powered By : [Devil Multiverse](https://t.me/DevilMultiverse).**
 """
     
     if plug_match:
